aba9857cb8dba6bdc8332848463e6093ad42ec6c
Alice <alicessr12@gmail.com>
2024-08-17 20:05:03 -0300

    Usando a classe

--- Sintaxe_indentação_c#/Program.cs
+++ Sintaxe_indentação_c#/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 //"dotnet new console" no terminal cria um novo projeto .net na pasta
 //ctrl shift ' abre um novo terminal ctrl ' abre o terminal que ja estava sendo utilizado
@@ -9,3 +8,13 @@ Console.WriteLine("Hello, World!");
 // .cs = classe, onde realiza o código, classe = conceito representado do mundo real(possui atributos e métodos)
 
 //todo nome de classe deve começar com maisculo, exemplo: PessoaFísica
+
+//Promgram.cs é o ponto de entrada do programa, começa a ler do program
+
+using Sintaxe_indentação_c_.models; //atravez do namespace irá reconhecer a classe pessoa
+
+Pessoa pessoa = new Pessoa();//instanciar a classe, p = variavel
+
+pessoa.Nome = "Alice"; //representa o set por estar passando um valor para nome
+pessoa.Idade = 21;
+pessoa.Apresentar();
--- Sintaxe_indentação_c#/models/Pessoa.cs
+++ Sintaxe_indentação_c#/models/Pessoa.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Sintaxe_indentação_c_.models
+namespace Sintaxe_indentação_c_.models//namespace = organização das classes, caminho lógico
 {
-    public class Pessoa
-    {
-        //prop = propriedade, propriedade = características que a pessoa vai ter
-        public String Nome { get; set; }
+    public class Pessoa //classe de nome pessoa
+    {//inicio classe
+        //prop = propriedade, propriedade = características que a pessoa vai ter (nome, idade), se a propriedade não for utilizada não tem sentido existir
+        public string? Nome { get; set; } //get = pegar valor, set = atribuir valor
+        // ? para deixar o nome anulavel
         public int Idade { get; set; }
-
-        //metodo = ação que a pessoa pode realizar
+        //metodo = ação que a pessoa pode realizar (função)
         public void Apresentar()
         {
             Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos");
+            //Console = classe, WriteLine = metodo, conteudo entre as chaves = parametro/argumento
         }
-    }
+    }//fim classe
 }

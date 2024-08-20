de4604851e83deaa6793046f8528003c929daf38
Alice <alicessr12@gmail.com>
2024-08-19 21:36:49 -0300

    tipos de dados

--- Sintaxe_indentação_c#/Program.cs
+++ Sintaxe_indentação_c#/Program.cs
@@ -7,14 +7,28 @@
 
 // .cs = classe, onde realiza o código, classe = conceito representado do mundo real(possui atributos e métodos)
 
-//todo nome de classe deve começar com maisculo, exemplo: PessoaFísica
-
 //Promgram.cs é o ponto de entrada do programa, começa a ler do program
 
 using Sintaxe_indentação_c_.models; //atravez do namespace irá reconhecer a classe pessoa
 
 Pessoa pessoa = new Pessoa();//instanciar a classe, p = variavel
+//nome da variaveis camelCase (sem caracteres especiais, somente _)
 
 pessoa.Nome = "Alice"; //representa o set por estar passando um valor para nome
 pessoa.Idade = 21;
 pessoa.Apresentar();
+
+//Tipos de dados mais utilizados: string, object, bool, decimal(valores monetários), double, int
+//variavel = valor alocado na memoria para ser utilizado no programa
+string apresentacao = "Ola, seja bem vindo";
+int quantidade = 1;
+double altura = 1.50; //cases decimais separadas por .
+decimal preco = 1.80m; //precisa colocar M ao passar diretamente o valor
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura);
+Console.WriteLine("Valor da variável preço: " + preco);
+Console.WriteLine("Valor da variável condição: " + condicao);
+
--- Sintaxe_indentação_c#/models/Pessoa.cs
+++ Sintaxe_indentação_c#/models/Pessoa.cs
@@ -6,16 +6,25 @@ using System.Threading.Tasks;
 namespace Sintaxe_indentação_c_.models//namespace = organização das classes, caminho lógico
 {
     public class Pessoa //classe de nome pessoa
+    //nome de classe utiliza PascalCase, não precisa ser o nome do arquivo físico mas é recomendado ou pelo menos ter relação (sem caracteres especiais, somente _)
     {//inicio classe
+
         //prop = propriedade, propriedade = características que a pessoa vai ter (nome, idade), se a propriedade não for utilizada não tem sentido existir
+        //nome de propriedade PascalCase, também não deve ser abreviado mesmo que o nome fique grande (sem caracteres especiais, somente _)
         public string? Nome { get; set; } //get = pegar valor, set = atribuir valor
         // ? para deixar o nome anulavel
         public int Idade { get; set; }
+
+
+
         //metodo = ação que a pessoa pode realizar (função)
         public void Apresentar()
+        //nome de metodo PascalCase, (sem caracteres especiais, somente _)
         {
             Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos");
             //Console = classe, WriteLine = metodo, conteudo entre as chaves = parametro/argumento
+            //  \n pula linha
         }
+        
     }//fim classe
 }
